ac110edd4037f764f9e90a413a3c14646e4d35c4
tranquangvu <vutran.513@gmail.com>
2016-02-19 01:11:37 +0700

    Add user for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
     @recipes = Recipe.all.order('created_at DESC')
@@ -9,11 +10,11 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
 
     if @recipe.save
       redirect_to @recipe, notice: 'Successfully create new recipe'
@@ -44,7 +45,7 @@ class RecipesController < ApplicationController
       params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
     end
 
-    def find_recipe
+    def set_recipe
       @recipe = Recipe.find(params[:id])
     end
 end
